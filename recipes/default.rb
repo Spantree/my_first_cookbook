a85a7a6f19a1d3f08fd187814dcf551cf6a6fcdd
Sebastian Otaegui <feniix@gmail.com>
2016-03-28 14:12:54 -0300

    change static file for template

--- recipes/default.rb
+++ recipes/default.rb
@@ -10,8 +10,8 @@ service 'httpd' do
   action [ :enable, :start ]
 end
 
-cookbook_file '/var/www/html/index.html' do
-  source 'index.html'
+template '/var/www/html/index.html' do
+  source 'index.html.erb'
   owner 'root'
   group 'root'
   mode '0644'
